f8eb7ea4a90cb70395e46489ca403843926e33c5
Sushanth2008 <67271099+Sushanth2008@users.noreply.github.com>
2021-12-04 17:45:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAudbMOkWYTaRqj2_-YAD-n6ZkHXE-DSIw",
+  authDomain: "sc-attendance-5e300.firebaseapp.com",
+  projectId: "sc-attendance-5e300",
+  storageBucket: "sc-attendance-5e300.appspot.com",
+  messagingSenderId: "107510497569",
+  appId: "1:107510497569:web:02abc5f5b739e2359bfe60"
+};
+
+export default firebaseConfig
+ 
+ 
+
+  
